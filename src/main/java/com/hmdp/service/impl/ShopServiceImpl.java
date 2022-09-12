37308eb3badf3d5576d3238ac7a5d2d781e09e65
Umky <zy.toy@qq.com>
2022-09-12 11:49:55 +0800

    feat: 为ShopType添加缓存

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,10 +1,16 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -17,4 +23,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
 
+    @Resource(name = "stringRedisTemplate")
+    ValueOperations<String, String> valueOperations;
+
+    @Override
+    public Result findById(Long id) {
+        String key = "cache:shop:" + id;
+        // visit cache
+        String shopJson = valueOperations.get(key);
+        if (StrUtil.isNotBlank(shopJson)) {
+            return Result.ok(JSONUtil.toBean(shopJson, Shop.class));
+        }
+        // visit db then cache
+        Shop shop = getById(id);
+        if (shop == null) {
+            return Result.fail("店铺不存在");
+        }
+        shopJson = JSONUtil.toJsonStr(shop);
+        valueOperations.set(key, shopJson);
+        return Result.ok(shop);
+    }
 }
--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,16 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +23,23 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    @Resource(name = "stringRedisTemplate")
+    ValueOperations<String, String> valueOperations;
+
+    @Override
+    public Result findAllType() {
+        String key = "cache:types";
+        // cache
+        String typeJsonArray = valueOperations.get(key);
+        if (typeJsonArray != null) {
+            return Result.ok(JSONUtil.toList(typeJsonArray, ShopType.class));
+        }
+        // visit db then cache
+        List<ShopType> typeList = query().orderByAsc("sort").list();
+        if(typeList.isEmpty()) {
+            return Result.fail("商品类型为空");
+        }
+        valueOperations.set(key, JSONUtil.toJsonStr(typeList));
+        return Result.ok(typeList);
+    }
 }
